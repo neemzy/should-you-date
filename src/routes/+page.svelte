05c92015a50f64d9c2db50aaa1999608efe2159d
Tom Panier <tom.panier@critizr.com>
2023-04-12 10:00:11 +0200

    tailwind

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,17 +1,29 @@
 <script>
+import classNames from "classnames";
+import "../app.css";
 import {checkAges, AgeResult} from "../checkAges";
 
 let askerAge = null;
 let otherAge = null;
+let result;
 let resultString;
-$: resultString = getResultString(askerAge, otherAge);
+$: result = getResult(askerAge, otherAge);
+$: resultString = getResultString(result);
 
-function getResultString(askerAge, otherAge) {
+function getResult(askerAge, otherAge) {
   if (!isValid(askerAge) || !isValid(otherAge)) {
-    return "";
+    return;
   }
 
-  switch (checkAges(parseInt(askerAge), parseInt(otherAge))) {
+  return checkAges(parseInt(askerAge), parseInt(otherAge));
+}
+
+function isValid(age) {
+  return !isNaN(age) && !isNaN(parseInt(age)) && age >= 0;
+}
+
+function getResultString(result) {
+  switch (result) {
     case AgeResult.ASKER_TOO_OLD:
       return "You're too old for them, leave them alone!";
     case AgeResult.ASKER_TOO_YOUNG:
@@ -25,28 +37,51 @@ function getResultString(askerAge, otherAge) {
     case AgeResult.ALL_GOOD:
       return "Right on! Have fun out there 😉"
   }
+
+  return "";
 }
 
-function isValid(age) {
-  return !isNaN(age) && !isNaN(parseInt(age)) && age >= 0;
+function isResultGood(result) {
+  return result === AgeResult.ALL_GOOD;
 }
 </script>
 
-<form>
-  <div>
-    <label for="askerAge">Your age:</label>
-    <input type="text" id="askerAge" bind:value={askerAge} />
-    {#if !isValid(askerAge)}
-      <span>Please enter a valid age for yourself</span>
-    {/if}
-  </div>
-  <div>
-    <label for="otherAge">Their age:</label>
-    <input type="text" id="otherAge" bind:value={otherAge} />
-    {#if !isValid(otherAge)}
-      <span>Please enter a valid age for the other person</span>
-    {/if}
-  </div>
-</form>
+<div class="p-4">
+  <h1 class="text-4xl mb-8">Should you date?</h1>
 
-<div>{resultString}</div>
+  <form>
+    <div class="flex mb-4">
+      <label for="askerAge" class="w-24 text-right">Your age:</label>
+      <div class="flex flex-col pl-4">
+        <input
+          type="text"
+          id="askerAge"
+          class="px-1 border border-solid border-slate-200 rounded"
+          bind:value={askerAge}
+        />
+        {#if !isValid(askerAge)}
+          <span class="text-xs text-red-500">Please enter a valid age for yourself</span>
+        {/if}
+      </div>
+    </div>
+    <div class="flex">
+      <label for="otherAge" class="w-24 text-right">Their age:</label>
+      <div class="flex flex-col pl-4">
+        <input
+          type="text"
+          id="otherAge"
+          class="px-1 border border-solid border-slate-200 rounded"
+          bind:value={otherAge}
+        />
+        {#if !isValid(otherAge)}
+          <span class="text-xs text-red-500">Please enter a valid age for the other person</span>
+        {/if}
+      </div>
+    </div>
+  </form>
+
+  <div class={classNames("mt-8 text-xl", {
+    "text-red-500": !isResultGood(result),
+    "text-green-500": isResultGood(result),
+  })}>{resultString}</div>
+</div>
